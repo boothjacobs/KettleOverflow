36b76f24f2735e7c21727c2c1d6d74b6de236da2
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-05-03 13:40:47 -0700

    users table

--- express-project-starter/app.js
+++ express-project-starter/app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const homeRouter = require('./routes/home');
 const usersRouter = require('./routes/users');
+const { sessionSecret } = require('./config');
 
 const app = express();
 
@@ -19,7 +20,7 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser(sessionSecret));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // set up session middleware
@@ -27,7 +28,7 @@ const store = new SequelizeStore({ db: sequelize });
 
 app.use(
   session({
-    secret: 'superSecret',
+    secret: sessionSecret,
     store,
     saveUninitialized: false,
     resave: false,
