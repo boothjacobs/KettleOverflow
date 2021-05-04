780b3dba64079c32c85b3d2bac92ada5c2dffe46
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-05-04 15:26:11 -0700

    home page updates

--- express-project-starter/routes/home.js
+++ express-project-starter/routes/home.js
@@ -6,7 +6,7 @@ const { csrfProtection, asyncHandler } = require('./utils');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log("We are on the home page ------------", res.locals.authenticated)
-  res.render('home', { title: 'a/A Express Skeleton Home' });
+  res.render('home', { title: 'Welcome Home' });
 });
 
 module.exports = router;
