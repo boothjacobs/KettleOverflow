df97ca2cd445ade97adc3230b72c55e7d6df59af
Sarah <jacobs.b.sarah@gmail.com>
2021-05-04 11:30:19 -0700

    seeder files

--- express-project-starter/db/seeders/20210504175817-Questions.js
+++ express-project-starter/db/seeders/20210504175817-Questions.js
@@ -2,9 +2,44 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    queryInterface.bulkInsert('Questions', [{
-        content: '',
-      }], {});
+    return queryInterface.bulkInsert('Questions', [
+      {
+        content: 'What is the best way to store loose-leaf teas?',
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        content: 'Should tea be kept in the freezer?',
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        content: 'How much caffeine is in green tea?',
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        content: 'What is the difference between herbal tea and decaffeinated tea?',
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        content: 'What is an Oolong?',
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        content: 'What is green tea and black tea?',
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+   ], {});
   },
 
   down: (queryInterface, Sequelize) => {
@@ -13,7 +48,7 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+      */
+   return queryInterface.bulkDelete('Questions', null, {});
   }
 };
