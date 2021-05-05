eced773b37f84a02ccc4d5fe748055dc973fce81
Sarah <jacobs.b.sarah@gmail.com>
2021-05-05 16:36:55 -0700

    vote notes

--- config/database.js
+++ config/database.js
@@ -12,7 +12,7 @@ module.exports = {
     seederStorage: 'sequelize',
   },
   production: {
-    use_env_variable: 'postgres://xibncwzqmsuaky:52e765fa044fab6ce1c916e863c373d42fb5563de2346111b725844921100adf@ec2-3-211-37-117.compute-1.amazonaws.com:5432/d9vrkck4c0a0dj',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
     dialectOptions: {
