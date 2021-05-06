65d710d671dcb43f034b2a5f18af580726b542e3
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-05-06 13:00:49 -0700

    answers changes

---
+++ public/javascripts/answer-edit.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    const editAnswer = document.querySelector('.edit-answer')
+
+    editAnswer.addEventListener('click', async (event) => {
+
+
+    })
+
+
+})
