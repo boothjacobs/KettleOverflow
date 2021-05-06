4de7c6ece3d73535c9efdde7b5f26e186dc314bc
Sarah <jacobs.b.sarah@gmail.com>
2021-05-06 09:45:57 -0700

    voting dependent on authentication

--- public/javascripts/voting-action.js
+++ public/javascripts/voting-action.js
@@ -3,18 +3,85 @@ const qDownvoteButton = document.getElementById("question-down-vote")
 const aUpvoteButton = document.getElementById("answer-up-vote")
 const aDownvoteButton = document.getElementById("answer-down-vote")
 
-qUpvoteButton.addEventListener("click", async (e) => {
+if (!res.locals.authenticated) {
+    qUpvoteButton.disabled = true;
+    qDownvoteButton.disabled = true;
+    aUpvoteButton.disabled = true;
+    aDownvoteButton.disabled = true;
+};
 
+qUpvoteButton.addEventListener("click", async (e) => {
+    const vote = true;
+    try {
+        const res = await fetch("/questions/:id/upVotes", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+            },
+        body: JSON.stringify(vote),
+        });
+
+        if (!res.ok) {
+          throw res;
+        }
+    } catch (err) {
+        alert(err.message)
+    }
 });
 
 qDownvoteButton.addEventListener("click", async (e) => {
+    const vote = false;
+    try {
+        const res = await fetch("/questions/:id/upVotes", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+            },
+        body: JSON.stringify(vote),
+        });
 
+        if (!res.ok) {
+          throw res;
+        }
+    } catch (err) {
+        alert(err.message)
+    }
 });
 
 aUpvoteButton.addEventListener("click", async (e) => {
+    const vote = true;
+    try {
+        const res = await fetch("/answers/:id/upVotes", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+            },
+        body: JSON.stringify(vote),
+        });
 
+        if (!res.ok) {
+          throw res;
+        }
+    } catch (err) {
+        alert(err.message)
+    }
 });
 
 aDownvoteButton.addEventListener("click", async (e) => {
+    const vote = false;
+    try {
+        const res = await fetch("/answers/:id/upVotes", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+            },
+        body: JSON.stringify(vote),
+        });
 
+        if (!res.ok) {
+          throw res;
+        }
+    } catch (err) {
+        alert(err.message)
+    }
 });
--- routes/voting.js
+++ routes/voting.js
@@ -1,22 +1,69 @@
 const express = require('express');
 const router = express.Router();
 const { loginUser, logoutUser, restoreUser } = require('../auth')
-const { User, Question, QuestionVote } = require('../db/models')
+const { User, Question, QuestionVote, AnswerVote } = require('../db/models')
 const { csrfProtection, asyncHandler } = require('./utils');
 const { Op } = require("sequelize");
 
+async function questionUpvotes(questionId) {
+    const tally = await QuestionVote.count({
+        where: {
+            upVote: true
+        }
+    })
+};
+
+async function questionDownvotes(questionId) {
+
+};
+
+async function answerUpvotes(answerId) {
+
+};
+
+async function answerDownvotes(answerId) {
+
+};
+
+
+
 router.post("/questions/:id/upVotes", asyncHandler(async (req, res) => {
+    const { userId } = req.session.auth;
+    const questionId = req.params.id;
+    const { vote } = req.body;
+
+    const voteExists = await QuestionVote.findOne({
+            where: { userId },
+            include: [Question, User]
+    });
+
+    if (voteExists) {
+        await voteExists.destroy();
+    } else {
+        await QuestionVote.create({
+            upVote: vote,
+            userId,
+            questionId
+        });
+    }
 
 }));
 
 router.post("/answers/:id/upVotes", asyncHandler(async (req, res) => {
+    const { userId } = req.session.auth;
+    const answerId = req.params.id;
+    const { vote } = req.body;
+    await AnswerVote.create({
+        upVote: vote,
+        userId,
+        answerId
+    });
+}));
+
+router.delete("/questions/:id/upVotes", asyncHandler(async (req, res) => {
 
 }));
 
-router.post("/questions/:id/upVotes", asyncHandler(async (req, res) => {
-
-}));
-
-router.post("/answers/:id/upVotes", asyncHandler(async (req, res) => {
+router.delete("/answers/:id/upVotes", asyncHandler(async (req, res) => {
 
 }));
